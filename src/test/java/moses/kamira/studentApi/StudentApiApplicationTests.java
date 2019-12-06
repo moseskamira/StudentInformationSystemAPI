80dc39a4086632f7def4de0661b64a34e12f3184
moseskamira <moses.african@gmail.com>
2019-12-06 14:13:40 +0300

    My First Spring Boot Commit

---
+++ src/test/java/moses/kamira/studentApi/StudentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package moses.kamira.studentApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
